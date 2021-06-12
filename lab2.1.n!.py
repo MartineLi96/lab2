fdcf4d92bc0c120d4f0f08f20d1604725cae2fda
MartineLi96 <82751420+MartineLi96@users.noreply.github.com>
2021-06-12 14:52:57 +0200

    Add files via upload

---
+++ lab2.1.n!.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 12 14:27:45 2021
+
+@author: MARTYNA
+"""
+
+n = 3
+silnia = 1
+i = 1
+while i <= n:
+    silnia = silnia * i
+    i = i + 1
+print ('silnia = ', silnia)
---
+++ lab2.2.n!.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+n=3
+silnia=1
+i=1
+print ('silnia =', silnia)
+print ('i =', i)
+while i<=n:
+    silnia = silnia * i
+    print ('silnia =', silnia)
+    i = i + 1
+    print ('i =', i)
+print ('koniec silnia = ', silnia)
+
